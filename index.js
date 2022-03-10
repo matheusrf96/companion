b5370fc454f5a0afabde7d9b47c177843d96a646
Matheus Rodrigues <matheusrf96@gmail.com>
2022-03-10 09:57:35 -0300

    Generate uuid

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 
+function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
 const socket = new WebSocket("ws://localhost:8000/ws")
 
 const data = {
+    uuid: uuidv4(),
     referrer: document.referrer,
     cookie: document.cookie,
     userAgent: window.navigator.userAgent,
